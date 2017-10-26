30d5b740a922857687be37f8ac25a93a82cbfb1e
amadodiazjr <amado.diaz.jr@gmail.com>
2017-10-26 17:55:04 -0400

    still compiles

--- Photorama/TagsViewController.swift
+++ Photorama/TagsViewController.swift
@@ -5,7 +5,7 @@ class TagsViewController: UITableViewController {
     var store: PhotoStore!
     var photo: Photo!
     
-    var selectedIndexPaths = [NSIndexPath]()
+    var selectedIndexPaths = [IndexPath]()
     
     let tagDataSource = TagDataSource()
     
@@ -13,13 +13,38 @@ class TagsViewController: UITableViewController {
         super.viewDidLoad()
         
         tableView.dataSource = tagDataSource
-        
+        tableView.delegate = self
         updateTags()
     }
     
     func updateTags() {
         let tags = try! store.fetchMainQueueTags(predicate: nil,
             sortDescriptors: [NSSortDescriptor(key: "name", ascending: true)])
-        //tagDataSource.tags = tags
+        tagDataSource.tags = tags
+        
+        for tag in photo.tags {
+            if let index = tagDataSource.tags.index(of: tag) {
+                let indexPath = IndexPath(row: index, section: 0)
+                selectedIndexPaths.append(indexPath)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let index = selectedIndexPaths.index(of: indexPath) {
+            selectedIndexPaths.remove(at: index)
+        } else {
+            selectedIndexPaths.append(indexPath)
+        }
+       
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if selectedIndexPaths.index(of: indexPath) != nil {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
     }
 }
